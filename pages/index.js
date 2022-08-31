0e3b948531c8e263021e56ce8e118c27d3c12cd7
kiyoshion <kiyoshion@gmail.com>
2022-09-01 07:28:32 +0900

    Update home

--- pages/index.js
+++ pages/index.js
@@ -3,9 +3,46 @@ import Image from 'next/image'
 import Link from 'next/link'
 import Layout from '../components/Layout'
 import styles from '../styles/Home.module.css'
+import utilStyles from '../styles/utils.module.css'
 
 export default function Home() {
   return (
-    <Layout></Layout>
+    <Layout>
+      <section className={utilStyles.headingMd}>
+        <p>I'm a freelance engineer.</p>
+      </section>
+
+      <section>
+        <h2>エンジニアブログ</h2>
+        <div className={styles.grid}>
+          <article>
+            <Link href="/">
+              <img
+                src="/img/thumbnail01.jpg"
+                className={styles.thumbnailImage}
+              />
+            </Link>
+            <Link href="/">
+              <a className={utilStyles.boldText}>記事1</a>
+            </Link>
+            <br />
+            <small className={utilStyles.lightText}>2022</small>
+          </article>
+          <article>
+            <Link href="/">
+              <img
+                src="/img/thumbnail01.jpg"
+                className={styles.thumbnailImage}
+              />
+            </Link>
+            <Link href="/">
+              <a className={utilStyles.boldText}>記事1</a>
+            </Link>
+            <br />
+            <small className={utilStyles.lightText}>2022</small>
+          </article>
+        </div>
+      </section>
+    </Layout>
   )
 }
